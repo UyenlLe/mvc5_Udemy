d6c1631326786ea4ba1decb6870da0b6a4545e78
Uyen Le <lebaouyen1@gmail.com>
2018-04-07 20:14:40 +0700

    Add new theme

--- UdemyProject/App_Start/BundleConfig.cs
+++ UdemyProject/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace UdemyProject
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-modern.css",
                       "~/Content/site.css"));
         }
     }

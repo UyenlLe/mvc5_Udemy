94410cad1e1cad5cee0d5bc3a9d82e33977755c1
Uyen Le <lebaouyen1@gmail.com>
2018-04-07 20:11:47 +0700

    Add MVC in Action example

---
+++ UdemyProject/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UdemyProject.Models;
+
+namespace UdemyProject.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            var movies = new Movies()
+            {
+                Name = "Ella"
+            };
+            return View(movies);
+        }
+    }
+}

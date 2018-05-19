241d73413e8aaf96a6bcb3187258946ca4ee3fdd
Uyen Le <lebaouyen1@gmail.com>
2018-05-19 10:31:25 +0700

    Install EE framework and create db context

---
+++ UdemyProject/Models/MyDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace UdemyProject.Models
+{
+    public class MyDBContext:DbContext
+    {
+        public MyDBContext()
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
